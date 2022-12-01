049ee22bf59bc04b5c7b7f7f1c04cabab72dff41
Christian Ingwersen <cin@trackman.com>
2022-12-01 06:20:53 +0100

    solutions to day1

---
+++ solutions/01/01.py
@@ -0,0 +1,16 @@
+import re
+
+## Read challenge input
+with open("solutions/01/input.txt") as f:
+    data = f.read()
+
+## split newlines and get ints from strings
+numbers = [sum(map(int, re.findall(r"-?\d+", x))) for x in data.split("\n\n")]
+
+# max value
+print(max(numbers))
+
+
+## Part 2
+# find the 3 largest numbers in list numbers and sum them
+print(sum(sorted(numbers)[-3:]))
